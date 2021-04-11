02f919022f228270290ce4496bf9ba0b7e5e6f46
xing.he <1245036094@qq.com>
2021-04-11 14:53:49 +0800

    fix: build errors

--- website/src/Question.js
+++ website/src/Question.js
@@ -3,9 +3,9 @@ import { useParams } from "react-router-dom";
 
 const Question = ()=>{
   const {id} = useParams();
-
   const [loading, setLoading] = useState(false);
   const [Component, setComponent] = useState(null);
+
   useEffect(()=>{
     if(!id || loading) return;
     setLoading(true);
@@ -13,7 +13,8 @@ const Question = ()=>{
       setComponent(C);
       setLoading(false);
     });
-  },[]);
+  },[loading, id]);
+  
   if(loading) return <div>Loading...</div>
   if(!Component) return <div>没有该题目</div>;
   return (<Component.default />);
