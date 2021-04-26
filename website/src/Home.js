8af1e7bc4c1433aa3c303524f70033dd1e3311e9
xing.he <1245036094@qq.com>
2021-04-26 14:39:05 +0800

    feat: update

--- website/src/Home.js
+++ website/src/Home.js
@@ -25,6 +25,11 @@ function Home() {
       <br/>
       <div><span>左右键 ← → 进行翻页，</span><span>回车键 ↩︎ 查看答案</span></div>
       <br/>
+      <div>
+        <p>Forked from <a href="https://github.com/lydiahallie/javascript-questions" target="_blank">lydiahallie/javascript-questions</a></p>
+        <p>从基础到进阶，测试你有多了解 JavaScript，刷新你的知识，或者帮助你的 coding 面试！</p>
+        <p>💪 🚀 祝你好运  ❤️</p>
+      </div>
       <Link to="/q/1">&nbsp;&nbsp;开始吧！</Link>
     </div>
   );
